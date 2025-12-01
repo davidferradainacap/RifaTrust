c748dd10bf7b9815f0bbe5fa3920ef8a98ffc9ca
David Ferrada <daldeaferrada@gmail.com>
2025-12-01 06:30:41 -0300

    Simplify startup.sh for Azure

--- startup.sh
+++ startup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Instalar dependencias
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-# Ejecutar migraciones
 python manage.py migrate --noinput
-
-# Colectar archivos estáticos
 python manage.py collectstatic --noinput
-
-# Iniciar Gunicorn
-gunicorn config.wsgi:application --bind=0.0.0.0:8000 --timeout 600 --workers 4
+gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers=2 config.wsgi:application
